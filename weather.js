523c5f164d46f2b7ea266589f5e612e7bcfa3ee0
Claudyu04 <81428418+Claudyu04@users.noreply.github.com>
2021-05-23 16:30:00 +0100

    small correction CSS and added weather api

--- weather.js
+++ weather.js
@@ -9,4 +9,18 @@ async function fetchWeather() {
 
   fetchWeather();
 
+// const newWeatherButton = document.getElementById("submitButton");
+// newWeatherButton.addEventListener("click", fetchWeather);
+// const ol = document.getElementById("showWeather");
+const getTemperature = dataResponse[1].main.temp
+console.log(getTemperature)
+
+// function addWeather(string){
+//     let li = document.createElement("li");
+//     let listItem = document.createTextNode(string)
+//     li.appendChild(listItem);
+//     ol.appendChild(li);
+    
+// }
+
   
